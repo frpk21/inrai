55141f86dff24e494b0f2bcf550b3b84cf4a9fb9
frpk21 <48810820+frpk21@users.noreply.github.com>
2023-03-14 15:11:53 -0500

    fotos

--- generales/admin.py
+++ generales/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Campanas,Categoria,Contacto,Nosotros
 
 class CampanasAdmin(admin.ModelAdmin):
-    list_display = ('categoria', 'titulo', 'archivo_foto','html','pdf', 'modificado', 'activo')
+    list_display = ('titulo', 'categoria', 'archivo_foto','html','pdf', 'modificado', 'activo')
     exclude = ('fecha_inicio_publicacion','fecha_final_publicacion', 'slug')
     ordering = ('categoria', '-titulo', '-modificado')
     search_fields = ('titulo',)
