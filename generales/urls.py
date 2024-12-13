dcf8373505eef713379e36b96dbd0ecdbde2dd55
frp.k21@hotmail.com <frp.k21@hotmail.com>
2024-12-13 12:24:13 -0500

    upd

--- generales/urls.py
+++ generales/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.Home.as_view(), name='home'),
-    path('customers/', views.Home.as_view(), name='customers'),
+    path('customers/', views.ConsultasClientesView.as_view(), name='customers'),
     path('login/', auth_views.LoginView.as_view(template_name='generales/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='generales/login.html'), name='logout'),
     path('loginunlock/', auth_views.LoginView.as_view(template_name='generales/lock.html'), name='loginunlock'),
