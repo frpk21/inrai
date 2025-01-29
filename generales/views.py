217b6e3f1d014c8f94fc1740f74bc7117418e9df
frp.k21@hotmail.com <frp.k21@hotmail.com>
2025-01-29 12:25:41 -0500

    opc send mail with products

--- generales/views.py
+++ generales/views.py
@@ -192,7 +192,7 @@ def enviar_correo(request):
                 "* SOLICITUD DE COTIZACIÓN *   **** PRUEBA APP WEB INRAI",  # Asunto del correo
                 mensaje,  # Cuerpo del correo
                 "administrador@sistemainrai.net",  # Cambia por tu correo
-                ["alejandra.cabrera@sistemainrai.net"],  # Cambia por el destinatario
+                ["alejandra.cabrera@sistemainrai.net","administrador@sistemainrai.net","heigel.charry@sistemainrai.net"],  # Cambia por el destinatario
              
             )
 
